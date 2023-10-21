ad01d4bf2847c80d4d0b91ac5d28eb0ab7eb6903
Ketil Malde <ketil@malde.org>
2023-10-21 23:04:06 +0200

    Validation checks object hashes using mmap and hashlib

--- medusa/__init__.py
+++ medusa/__init__.py
@@ -1,11 +1,33 @@
 from lxml import etree
 from os.path import dirname
+import mmap
+import hashlib
 
-def validate(manifest):
+def validate(dataset):
+    status = None
     libdir = dirname(dirname(__file__))+'/xml/'
     with open(f'{libdir}/manifest.rng', 'r') as f:
         schema = etree.RelaxNG(etree.parse(f))
-    with open(manifest, 'r') as f:
+    with open(f'{dataset }/manifest.xml', 'r') as f:
         doc = etree.parse(f)
-    return schema.validate(doc)
+    status = schema.validate(doc)
+
+    # data-specific validation (e.g. flowcam)
+
+    # Check object hashes:
+    # madvise(mmap.MADV_SEQUENTIAL)
+    for obj in doc.iter('object'):
+        fname = f'{dataset}/{obj.attrib["path"]}'
+        with open(fname, 'rb') as fh:
+            # is this effcient?  newer hashlib supports file_digest, probably better
+            with mmap.mmap(fh.fileno(), length=0, access=mmap.ACCESS_READ) as fm:
+                h = hashlib.sha1(fm).hexdigest()
+                if h != obj.attrib['sha1']:
+                    print('ERROR: checksum mismatch for {fname}, got {h}, wanted {obj.attrib["sha1"]}!')
+                    status = False
+                else:
+                    print(f'   "{fname}" - checksum OK')
+                # compare sha1.hexdigest() to the stored value
+                
+    return status
     
