9127fc38af4bc4b6a1142147dc17e251516bd330
Ketil Malde <ketil@malde.org>
2025-08-20 14:16:46 +0200

    Better infotext in generated metadata templates.

--- medusa/template.py
+++ medusa/template.py
@@ -3,6 +3,7 @@ from medusa.util import get_hash
 import datetime
 
 def create_template(dirname, name=None, date=None, author=None, description=None, provenance=None):
+    info = not name
     if not name:
         name = os.path.basename(dirname)
     if not date:
@@ -25,25 +26,29 @@ def create_template(dirname, name=None, date=None, author=None, description=None
             print(f'Processing {f} failed: {e}')
 
     with open(f'{dirname}/manifest.xml', 'w') as f:
-        f.write(header(name=name, date=date, author=author))
+        f.write(header(name=name, date=date, author=author, info=info))
         f.write(mkdescription(description))
         f.write(mkprovenance(provenance))
         f.write(objects(objs))
         f.write(footer())
 
-def header(name='...', date='...', author='...', cls=None):
+def header(name=None, date=None, author=None, cls=None, info=False):
     myclass = '' if cls is None else f' class="{cls}"'
-    return f'''<manifest name="{name}" created="{date}" author="{author}"{myclass} >
-  <!-- name is an arbitrary string, dates should be ISO format, person
-  is a valid email(?), and class gives hints to the validator on
-  what needs to be present -->
+    hd = f'<manifest name="{name}" created="{date}" author="{author}"{myclass} >\n'
+
+    if info:
+        hd += '''  <!-- name is an arbitrary string, dates should be ISO format, person
+       is a valid email(?), and class gives hints to the validator on
+       what needs to be present -->
 '''
+    else: print(name, date, author)
+    return hd
 
 def mkdescription(desc=None):
     if desc and os.path.exists(desc):
         with open(desc, 'r') as f:
             desctxt = f.read()
-        return '  <description>\n' + desctxt + '\n  </description>\n'
+        return '  <description>\n' + desctxt + '  </description>\n'
     else: return '''  <description>
     <!-- Just a textual description.  May contain XML elements
          referring to specific types of information (including other
@@ -58,7 +63,7 @@ def mkprovenance(prov=None):
     if prov and os.path.exists(prov):
         with open(prov, 'r') as f:
             provtxt = f.read()
-        return '  <provenance>\n' + provtxt + '\n  </provenance>\n'
+        return '  <provenance>\n' + provtxt + '  </provenance>\n'
     else: return '''  <provenance>
     <!-- Also text, describing the origins of the data, but with a bit
          more structure. E.g. you can use <process name="..." version="..." git-hash="..." />
